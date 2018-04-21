e192deaef17663be8c48ad24f1220ab1d75891b4
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-21 21:30:51 +0000

    Add index on tweet_id

--- sql/leogalle_test/retweet_log/create.sql
+++ sql/leogalle_test/retweet_log/create.sql
@@ -2,5 +2,6 @@ CREATE TABLE `retweet_log` (
     `retweet_log_id` int(10) unsigned auto_increment,
     `tweet_id` bigint(20) unsigned not null,
     `created` datetime not null default current_timestamp,
-    PRIMARY KEY (`retweet_log_id`)
+    PRIMARY KEY (`retweet_log_id`),
+    INDEX `tweet_id` (`tweet_id`)
 ) charset=utf8;
